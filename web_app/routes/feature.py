7c971041e51288b5382254ad2d4011dc32d6ef16
ethan <e19920526@gmail.com>
2025-12-24 15:22:00 +0800

    feat: 新增favorite table的函式

--- web_app/routes/feature.py
+++ web_app/routes/feature.py
@@ -1,28 +1,49 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from web_app.models.feature import FavoriteRestaurant
 from web_app.mysql_connection import get_db_cursor
+import pymysql
+
 
 router = APIRouter()
 
-@router.post('/favorite')
+# 建立table函式
+def create_table(cursor):
+    create_query = f"""
+        create table favorite(
+            fav_id int primary key auto_increment,
+            user_id int not null,
+            restaurant_id int not null,
+            fav_note varchar(300)
+        )
+        """
+    cursor.execute("show tables like %s", ("favorite"))
+    result = cursor.fetchone()
+
+    # 當沒有table時才建立
+    if result is None:
+        try:
+            cursor.execute(create_query)
+            print(f"favorite table is created!!")
+        except pymysql.Error as e:
+            print(f"Error create favorite table: {e}")
+
+@router.post("/favorite")
 def add_favorite(favorite: FavoriteRestaurant):
     try:
-        # 提交資料要commit=True
+        # 在favorite中放入資料
+        # 注意:提交資料要commit記得設為True
         with get_db_cursor(commit=True) as cursor:
-            sql = "insert into favorite(user_id, restaurant_id, fav_note) values(%s, %s, %s)"
-            
-            cursor.execute(sql, (favorite.user_id, favorite.restaurant_id, favorite.fav_note))
-            
-            cursor.execute("select * from favorite")
-            
-            new_data = cursor.fetchall()
-            
+            create_table(cursor)
+            sql = "insert into favorite(user_id, restaurant_id, fav_note) values(%s, %s, %s)"           
+            cursor.execute(sql, (favorite.user_id, favorite.restaurant_id, favorite.fav_note))           
+            cursor.execute("select * from favorite")            
+            new_data = cursor.fetchall()            
             return {
                 "success": True,
                 "data": new_data
             }
     except Exception as e:
-        print("error", e)
+        print(f"Error is happening: {e}")
     
     
     
