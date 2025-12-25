a6e7ba586a81447cd7f1f5cc24e0df703f7e7f9a
ethan <e19920526@gmail.com>
2025-12-25 15:51:55 +0800

    feat: 新增查詢餐廳路由

--- web_app/routes/feature.py
+++ web_app/routes/feature.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Path, HTTPException
 from web_app.models.feature import FavoriteRestaurant
 from web_app.mysql_connection import get_db_cursor
 import pymysql
+from typing import Annotated
 
 
 router = APIRouter()
@@ -27,6 +28,7 @@ def create_table(cursor):
         except pymysql.Error as e:
             print(f"Error create favorite table: {e}")
 
+# 新增收藏餐廳路由
 @router.post("/favorite")
 def add_favorite(favorite: FavoriteRestaurant):
     try:
@@ -36,15 +38,29 @@ def add_favorite(favorite: FavoriteRestaurant):
             create_table(cursor)
             sql = "insert into favorite(user_id, restaurant_id, fav_note) values(%s, %s, %s)"           
             cursor.execute(sql, (favorite.user_id, favorite.restaurant_id, favorite.fav_note))           
-            cursor.execute("select * from favorite")            
-            new_data = cursor.fetchall()            
             return {
-                "success": True,
-                "data": new_data
+                "status": "Success"
             }
     except Exception as e:
-        print(f"Error is happening: {e}")
+        raise HTTPException(status_code=500, detail=f"資料庫錯誤: {e}")
+
+# 查詢收藏餐廳路由
+@router.get("/favorite/{user_id}")
+# 設定user_id必須大於0
+def get_favorite(user_id: Annotated[int, Path(title="The ID of user", gt=0)]):
+    try:
+        with get_db_cursor() as cursor:
+            sql = "select * from favorite where user_id=%s"
+            cursor.execute(sql, (user_id))
+            results = cursor.fetchall()
+        return {
+            "status": "Success",
+            "user_id": user_id,
+            "results": results
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"資料庫錯誤: {e}")
     
-    
-    
+
+
         
