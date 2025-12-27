92c24a88e159c1703df99cc14cab75aac2e13c47
ethan <e19920526@gmail.com>
2025-12-27 15:18:33 +0800

    fix: 新增CORS設定,讓前端vue可訪問後端資料庫

--- web_app/main.py
+++ web_app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from web_app.routes.feature import router as favorite_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# CORS設定
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # production環境要限制特定域名
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.get("/")
 def root():
