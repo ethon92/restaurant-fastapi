ad8f3d53f55f33e7f063de7016f9693955912970
hazellin00 <hazellin63@gmail.com>
2026-01-06 11:01:57 +0800

    revise the datetime from schema

--- web_app/models/schema.py
+++ web_app/models/schema.py
@@ -1,11 +1,12 @@
 from pydantic import BaseModel
 from typing import Optional
+from datetime import date, time
 
 class ReservationRequest(BaseModel):
     restaurant_name: str
     user_name: str
     phone: str
-    date: str
-    time: str
+    date: date
+    time: time
     people: int
     note: Optional[str] = ""
