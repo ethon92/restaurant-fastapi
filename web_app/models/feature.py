3f76669941d92a112ffc16606b92ed3ccb23180f
張曜安 <chang.yao.an@gmail.com>
2026-01-13 10:25:59 +0800

    新增餐廳評論路由

--- web_app/models/feature.py
+++ web_app/models/feature.py
@@ -4,3 +4,8 @@ class FavoriteRestaurant(BaseModel):
     user_id: int
     restaurant_id: str
     fav_note: str | None
+    
+class RestaurantComment(BaseModel):
+    user_id: int
+    restaurant_id: str
+    comment_content: str
