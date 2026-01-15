7ebd4209063d1a6d569b5d3d84bad36eb3cd807d
張曜安 <chang.yao.an@gmail.com>
2026-01-15 13:57:29 +0800

    更新查詢評論(餐廳)

--- web_app/routes/restaurant.py
+++ web_app/routes/restaurant.py
@@ -5,6 +5,28 @@ from typing import Annotated
 
 router = APIRouter()
 
+# 建立comment的table
+def create_comment_table(cursor):
+    create_query = """
+    create table comment (
+        comment_id int primary key auto_increment,
+        user_id int not null,
+        restaurant_id varchar(50) not null,             
+        comment_content varchar(255) not null,   
+        rating int not null check(rating >= 1 AND rating <= 5),              
+        comment_time DATETIME DEFAULT CURRENT_TIMESTAMP 
+    );
+    """
+    cursor.execute("show tables like %s", ("comment"))
+    result = cursor.fetchone()
+    # 當沒有table時才建立
+    if result is None:
+        try:
+            cursor.execute(create_query)
+            print("comment table is created!!")
+        except pymysql.Error as e:
+            print(f"Error create comment table: {e}")
+
 # 查詢評論餐廳路由(根據 restaurant ID)
 @router.get("/comment/{restaurant_id}")
 
