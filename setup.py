b831d1dc6dd7bd676748b70ae26f5bb3708d2403
ethan <e19920526@gmail.com>
2026-01-18 15:09:54 +0800

    feature: 新增建立資料庫設定檔

---
+++ setup.py
@@ -0,0 +1,82 @@
+from web_app.mysql_connection import get_db_cursor
+import pymysql
+from fastapi import HTTPException
+
+
+class setup_database:
+    def __init__(self, name, sql, cursor):
+        self.name = name
+        self.sql = sql
+        self.cursor = cursor
+
+    def create_table(self):
+        cursor.execute("show tables like %s", (f"{self.name}"))
+        result = cursor.fetchone()
+
+        # 當沒有table時才建立
+        if result is None:
+            try:
+                cursor.execute(self.sql)
+                print(f"{self.name} table is created!!")
+            except pymysql.Error as e:
+                print(f"Error create favorite table: {e}")
+        else:
+            print(f"{self.name} table exits!!")
+
+
+create_comments = """create table comments(
+        comment_id int primary key auto_increment,
+        user_id int not null,
+        restaurant_id varchar(50) not null,             
+        comment_content varchar(255) not null,   
+        rating int not null check(rating >= 1 AND rating <= 5),
+        comment_time DATETIME DEFAULT CURRENT_TIMESTAMP               
+    )"""
+
+create_favorite = """
+        create table favorite(
+            fav_id int primary key auto_increment,
+            user_id int not null,
+            restaurant_id varchar(50) not null,
+            fav_note varchar(300)
+        )
+        """
+
+create_users = """
+        CREATE TABLE `users` (
+                `user_id` INTEGER NOT NULL AUTO_INCREMENT,
+                `user_name` VARCHAR(20),
+                `user_email` VARCHAR(30) NOT NULL,
+                `user_password` VARCHAR(255) NOT NULL,
+                `user_birthday` DATE NOT NULL,
+                `user_role` BOOLEAN DEFAULT 0,
+                PRIMARY KEY (`user_id`),
+                UNIQUE KEY `uq_users_email` (`user_email`)
+        );
+"""
+
+create_reservations = """
+        CREATE TABLE reservations (
+            booking_id INT AUTO_INCREMENT PRIMARY KEY,
+            restaurant_name VARCHAR(100),
+            user_id int,
+            user_name VARCHAR(50),
+            phone VARCHAR(20),
+            party_size INT,
+            booking_time VARCHAR(50),
+            booking_status VARCHAR(50) DEFAULT 'confirmed',
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            note text
+    );
+"""
+
+try:
+    with get_db_cursor(commit=True) as cursor:
+        setup_database("comments", create_comments, cursor).create_table()
+        setup_database("favorite", create_favorite, cursor).create_table()
+        setup_database("users", create_users, cursor).create_table()
+        setup_database("reservations", create_reservations, cursor).create_table()
+except HTTPException:
+    raise
+except Exception as e:
+    raise HTTPException(status_code=500, detail=f"資料庫錯誤: {e}")
